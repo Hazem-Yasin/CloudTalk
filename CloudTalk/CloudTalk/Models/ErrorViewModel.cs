e43e06a91631d0f0e438b947f60dc445816fcc4f
Hazem-Yasin <hazem.yasin.1416@gmail.com>
2024-04-12 16:44:03 +0200

    started the project

---
+++ CloudTalk/CloudTalk/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CloudTalk.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
